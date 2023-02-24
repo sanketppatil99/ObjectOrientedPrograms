b8a1b3580cd630fe0ea40874e99ae3f99e10fdf4
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 05:22:10 +0530

    JSON Inventory data management

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Object Oriented Programs");
-            
-           
+            InventoryManagement.FetchInvertory json = new InventoryManagement.FetchInvertory();
+            json.convert("D:\\257\\JsonInventory\\InventoryManagement\\Inventory.json");
+
         }
     }
 }
