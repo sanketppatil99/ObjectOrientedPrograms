aaee108cfc1934e24556e80ca7c0b3d64e652972
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 05:25:31 +0530

    JSON Inventory data management

---
+++ InventoryDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    { 
+        public string name { get; set; }
+        public int weight { get; set; }
+
+        public int price { get; set; }
+    }
+}
+
+   
+
