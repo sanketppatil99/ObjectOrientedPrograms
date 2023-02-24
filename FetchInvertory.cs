888792a0c7d2d12fd43f2ed4c3cab1734b42d807
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 05:26:59 +0530

    JSON Inventory data management

---
+++ FetchInvertory.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class FetchInvertory
+    {
+        InventoryDetails Inventory = new InventoryDetails();
+        List<InventoryDetails> InventoryList = new List<InventoryDetails>();
+
+        public void convert(string jFilePath)
+        {
+            using (StreamReader reader = new StreamReader(jFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<InventoryDetails>>(json);
+                Console.WriteLine("Name weight Price Total value");
+
+                foreach (var item in items)
+                {
+                    double value = item.price * item.weight;
+
+                    Console.WriteLine(item.name + " " + item.weight + " " + item.price + " " + value);
+                }
+            }
+        }
+    }
+}
+
+
