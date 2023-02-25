174e8120cd79fe76602297419b5fac56361722c3
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 05:59:34 +0530

    JSON Stock account managment

---
+++ StockAccount/StockCalculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccount
+{
+    public class StockCalculation
+    {
+        public List<StockAccount> stockAccount;
+    }
+    public class StockAccount
+    {
+        public string shareName { get; set; }
+
+        public int numberOfShare { get; set; }
+
+        public int sharePrice { get; set; }
+
+    }
+}
