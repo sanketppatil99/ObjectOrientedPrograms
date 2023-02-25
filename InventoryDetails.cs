fcc88a17c4561d5cacf69b85ff962bd3056d2ce0
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 05:39:36 +0530

    JSON Inventory  management

--- InventoryDetails.cs
+++ InventoryDetails.cs
@@ -8,11 +8,32 @@ using System.Threading.Tasks;
 namespace InventoryManagement
 {
     public class InventoryDetails
-    { 
+    {
+        public List<TypeOfRice> typeOfRice;
+        public List<TypeOfPulses> typeOfPulses;
+        public List<TypeOfWheat> typeOfWheat;
+    }
+
+    public class TypeOfRice
+    {
         public string name { get; set; }
         public int weight { get; set; }
-
         public int price { get; set; }
+
+    }
+    public class TypeOfPulses
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+    public class TypeOfWheat
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
     }
 }
 
